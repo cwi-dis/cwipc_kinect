1690a62e237b3acf96acf3580467409ec9bfaf5a
Jack Jansen <Jack.Jansen@cwi.nl>
2022-04-05 01:28:30 +0200

    Allow specifying K4A_ROOT and K4ABT_ROOT on windows

--- CMakeFiles/FindKINECT.cmake
+++ CMakeFiles/FindKINECT.cmake
@@ -14,15 +14,20 @@
 if(WIN32)
 
   set(CMAKE_FIND_LIBRARY_SUFFIXES .dll ${CMAKE_FIND_LIBRARY_SUFFIXES})
-
+  if(NOT K4A_ROOT)
+    set(K4A_ROOT "C:/Program Files/Azure Kinect SDK v1.4.1")
+  endif()
+  if(NOT K4ABT_ROOT)
+    set(K4ABT_ROOT "C:/Program Files/Azure Kinect Body Tracking SDK")
+  endif()
 endif()
 
 #
 # Find Kinect for Azure SDK
 # xxxjack manual hardcoded pathname for now.
 if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
-	set(KINECT_LIBRARY_DIRS "C:/Program Files/Azure Kinect SDK v1.4.1/sdk/windows-desktop/amd64/release/lib" "C:/Program Files/Azure Kinect Body Tracking SDK/sdk/windows-desktop/amd64/release/lib")
-	set(KINECT_INCLUDE_DIRS "C:/Program Files/Azure Kinect SDK v1.4.1/sdk/include" "C:/Program Files/Azure Kinect Body Tracking SDK/sdk/include")
+	set(KINECT_LIBRARY_DIRS "${K4A_ROOT}/sdk/windows-desktop/amd64/release/lib" "${K4ABT_ROOT}/sdk/windows-desktop/amd64/release/lib")
+	set(KINECT_INCLUDE_DIRS "${K4A_ROOT}/sdk/include" "${K4ABT_ROOT}/sdk/include")
 	set(KINECT_LIBRARIES "k4a" "k4arecord" "k4abt")
 elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
 	set(KINECT_LIBRARY_DIRS "/usr/lib/x86_64-linux-gnu")
