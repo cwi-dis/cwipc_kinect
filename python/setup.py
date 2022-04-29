08aa63bd9024a37ac2e1d435302c4c72ae9f547c
Jack Jansen <Jack.Jansen@cwi.nl>
2022-04-29 12:21:29 +0200

    Get rid of reference to old cwipc_kinectcalibrate. Partial fix for cwipc#5.

--- python/setup.py
+++ python/setup.py
@@ -159,11 +159,8 @@ setup(
     #
     # For example, the following would provide a command called `sample` which
     # executes the function `main` from this package when invoked:
-    entry_points={  # Optional
-        'console_scripts': [
-            'cwipc_kinectcalibrate=_cwipc_kinect.scripts.cwipc_kinectcalibrate:main',
-        ],
-    },
+    # entry_points={  # Optional
+    # },
 
     # List additional URLs that are relevant to your project as a dict.
     #
