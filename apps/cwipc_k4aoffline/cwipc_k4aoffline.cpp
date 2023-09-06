9600d6cfaf5517b57d6ba91dd435fb1f7e95b4b9
Thomas Röggla <t.roggla@cwi.nl>
2023-09-06 17:50:34 +0200

    reformatted all source files in apps/ directory

--- apps/cwipc_k4aoffline/cwipc_k4aoffline.cpp
+++ apps/cwipc_k4aoffline/cwipc_k4aoffline.cpp
@@ -9,16 +9,20 @@
 
 #undef DEBUG_AUXDATA
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     if (argc < 3) {
         std::cerr << "ERROR. Usage: " << argv[0] << " inputdirectory outputdirectory [count]" << std::endl;
         std::cerr << "Generates pointclouds from kinect4a camera recordings using the cameraconfig.xml" << std::endl;
         std::cerr << "If no outputdirectory a subfolder is created in the current folder" << std::endl;
         return 2;
     }
+
     int countWanted = 0;
-    if (argc > 3) countWanted = atoi(argv[3]);
+
+    if (argc > 3) {
+        countWanted = atoi(argv[3]);
+    }
+
     char filename[500];
     char* error = NULL;
     cwipc_tiledsource* generator;
@@ -30,11 +34,14 @@ int main(int argc, char** argv)
     configFile += "/cameraconfig.xml";
 
     generator = cwipc_k4aoffline(configFile.c_str(), &error, CWIPC_API_VERSION);
+
     if (generator == NULL) {
         std::cerr << argv[0] << ": creating offlinekinect grabber failed: " << error << std::endl;
         //if (getenv("CWIPC_KINECT_TESTING") != NULL) return 0; // No failure while running tests, so we can at least test linking, etc.
+
         return 1;
     }
+
     if (error) {
         std::cerr << argv[0] << ": warning while creating kinect grabber: " << error << std::endl;
     }
@@ -48,34 +55,46 @@ int main(int argc, char** argv)
     int ok = 0;
     int framenum = 0;
     int nGrabbedSuccessfully = 0;
+
     while (!generator->eof()) {
-        if (countWanted != 0 && framenum >= countWanted) break;
+        if (countWanted != 0 && framenum >= countWanted) {
+            break;
+        }
+
         cwipc* pc = NULL;
         pc = generator->get();
+
         if (pc == NULL) {
             error = (char*)"grabber returned NULL pointcloud";
             ok = -1;
+
             break;
         }
+
         if (pc->count() <= 0) {
             std::cerr << argv[0] << ": warning: empty pointcloud, grabbing again" << std::endl;
             pc->free();
+
             continue;
         }
+
 #ifdef DEBUG_AUXDATA
         cwipc_auxiliary_data* ap = pc->access_auxiliary_data();
+
         if (ap == nullptr) {
             std::cerr << argv[0] << ": access_auxiliary_data: returned null pointer" << std::endl;
-        }
-        else {
+        } else {
             std::cerr << argv[0] << ": auxdata: " << ap->count() << " items:" << std::endl;
+
             for (int i = 0; i < ap->count(); i++) {
                 void* ptr = ap->pointer(i);
                 std::cerr << argv[0] << "auxdata: item " << i << " name=" << ap->name(i) << ", size=" << (int)ap->size(i) << ", descr=" << ap->description(i) << ", pointer=0x" << std::hex << (uint64_t)ptr << std::dec << std::endl;
             }
         }
 #endif
+
         framenum++;
+
 		if (strcmp(outputdir, "-") != 0) {
 			snprintf(filename, sizeof(filename), "%s/pointcloud-%08.8" PRIu64 ".ply", outputdir, pc->timestamp());
 	        std::cout << "-> Writing frame " << framenum << " with " << pc->count() << " points to "<< filename << std::endl;
@@ -83,18 +102,23 @@ int main(int argc, char** argv)
 		} else {
 	        std::cout << "-> Dropping frame " << framenum << " with " << pc->count() << " points" << std::endl;
 		}
+
         pc->free();
         nGrabbedSuccessfully++;
     }
+
     generator->free();
+
     if (ok < 0) {
         std::cerr << "cwipc_k4aoffline: Error: " << error << std::endl;
         return 1;
     }
+
     if (countWanted != 0 && nGrabbedSuccessfully != countWanted) {
         std::cerr << "cwipc_k4aoffline: Wanted " << countWanted << " pointclouds but got only " << nGrabbedSuccessfully << std::endl;
         return 1;
     }
+
     std::cerr << "cwipc_k4aoffline: Saved " << nGrabbedSuccessfully << " pointclouds" << std::endl;
     return 0;
 }
--- apps/cwipc_k4aoffline_check/cwipc_k4aoffline_check.cpp
+++ apps/cwipc_k4aoffline_check/cwipc_k4aoffline_check.cpp
@@ -8,8 +8,7 @@
 #include <k4a/k4a.h>
 #include <k4arecord/playback.h>
 
-typedef struct
-{
+typedef struct {
 	char* filename;
 	k4a_playback_t handle;
 	k4a_record_configuration_t record_config;
@@ -29,10 +28,9 @@ bool check_color_and_depth(recording_t* file) {
     k4a_image_t color = k4a_capture_get_color_image(file->capture);
     k4a_image_t depth = k4a_capture_get_depth_image(file->capture);
 
-    if((color == NULL) || (depth == NULL)){
+    if ((color == NULL) || (depth == NULL)){
         return false;
-    }
-    else {
+    } else {
         file->current_capture_timestamp = k4a_image_get_device_timestamp_usec(color);
         return true;
     }
@@ -41,51 +39,60 @@ bool check_color_and_depth(recording_t* file) {
 bool prepare_next_valid_frame(recording_t* file) {
     k4a_result_t result;;
     k4a_stream_result_t stream_result;
+
     // Read the next capture into memory
     bool succeeded = false;
     while (!succeeded){
-        if (file->capture != NULL)
+        if (file->capture != NULL) {
             k4a_capture_release(file->capture);
+        }
+
         stream_result = k4a_playback_get_next_capture(file->handle, &file->capture);
-        if (stream_result == K4A_STREAM_RESULT_EOF)
-        {
+
+        if (stream_result == K4A_STREAM_RESULT_EOF) {
             if (file->current_capture_timestamp == 0) {
                 std::cerr << "ERROR: Recording file is empty: "<< file->filename << std::endl;
                 result = K4A_RESULT_FAILED;
-            }
-            else {
+            } else {
                 std::cout << "Recording file " << file->filename << " reached EOF" << std::endl;
                 eof = true;
             }
+
             break;
-        }
-        else if (stream_result == K4A_STREAM_RESULT_FAILED)
-        {
+        } else if (stream_result == K4A_STREAM_RESULT_FAILED) {
             std::cerr << "ERROR: Failed to read first capture from file: " <<  file->filename << std::endl;
             result = K4A_RESULT_FAILED;
+
             break;
         }
+
         file->capture_id++;
 
         k4a_image_t color = k4a_capture_get_color_image(file->capture);
         if (color == NULL) {
-            if(!first)
+            if(!first) {
                 std::cerr << "Color is missing in capture " << file->capture_id << " from " << file->filename << std::endl;
+            }
+
             continue;
         }
 
         k4a_image_t depth = k4a_capture_get_depth_image(file->capture);
         if (depth == NULL) {
-            if (!first)
+            if (!first) {
                 std::cerr << "Depth is missing in capture " << file->capture_id << " from " << file->filename << std::endl;
+            }
+
             k4a_image_release(color);
             continue;
         }
+
         file->current_capture_timestamp = k4a_image_get_device_timestamp_usec(color);
         succeeded = true;
         k4a_image_release(color);
         k4a_image_release(depth);
     }
+
     return succeeded;
 }
 
@@ -98,47 +105,41 @@ int prepare_cond_next_valid_frame(recording_t* file, uint64_t master_timestamp, 
     /// <param name="max_delay"></param>
     /// <returns></returns>
     bool satisfies_condition = false;
+
     //check if there is a current frame that already satisfies the condition
     if (file->capture != NULL && (file->current_capture_timestamp > master_timestamp)) {
-        if (file->current_capture_timestamp < (master_timestamp + max_delay)) //satisfies
-        {
+        if (file->current_capture_timestamp < (master_timestamp + max_delay)) //satisfies {
             return 1;
-        }
-        else { //update master
+        } else { //update master
             return 2;
         }
-        
     }
+
     //otherwise start process to find a frame that satisfies the condition.
-    while (!satisfies_condition)
-    {
+    while (!satisfies_condition) {
         bool ok = prepare_next_valid_frame(file);
-        if (!ok) break;
+
+        if (!ok) {
+            break;
+        }
+
         if (file->current_capture_timestamp > master_timestamp) {
             if (file->current_capture_timestamp < (master_timestamp + max_delay)) {
                 satisfies_condition = true;
                 return 1;
-            }
-            else {  //it is a future frame, we need to update master frame
+            } else {  //it is a future frame, we need to update master frame
                 return 2;
             }
         }
     }
+
     return -1;
 }
 
-int main(int argc, char* argv[])
-{
-    /*
-    * //for external debugging
-    char c;
-    std::cin >> c;*/
-
-
+int main(int argc, char* argv[]) {
     bool ok;
 
-	if (argc < 3)
-	{
+	if (argc < 3) {
         std::cerr << "Usage: cwipc_k4aoffline_check <master.mkv> <sub1.mkv> ..." << std::endl;
 		std::cerr << "Convert multiple mkvfiles into pointclouds" << std::endl;
 		return 1;
@@ -151,109 +152,112 @@ int main(int argc, char* argv[])
 	// Allocate memory to store the state of N recordings.
 
 	recording_t* files = (recording_t*) malloc(sizeof(recording_t) * file_count);
-	if (files == NULL)
-	{
+	if (files == NULL) {
 		std::cerr << "Failed to allocate memory for playback (" << sizeof(recording_t) * file_count << " bytes)" << std::endl;
 		return 1;
 	}
+
 	memset(files, 0, sizeof(recording_t) * file_count);
 
     // Open each recording file and validate they were recorded in master/subordinate mode.
-    for (size_t i = 0; i < file_count; i++)
-    {
+    for (size_t i = 0; i < file_count; i++) {
         files[i].filename = argv[i + 1];
-        
+
         /*char rr;
         size_t vs*/;
         //k4a_buffer_result_t = k4a_playback_get_tag(files[i].handle, "K4A_COLOR_FIRMWARE_VERSION", &rr, &vs);
 
         result = k4a_playback_open(files[i].filename, &files[i].handle);
 
-        if (result != K4A_RESULT_SUCCEEDED)
-        {
+        if (result != K4A_RESULT_SUCCEEDED) {
             std::cerr << "Failed to open file: " << files[i].filename << std::endl;
             break;
         }
 
         result = k4a_playback_get_record_configuration(files[i].handle, &files[i].record_config);
-        if (result != K4A_RESULT_SUCCEEDED)
-        {
+        if (result != K4A_RESULT_SUCCEEDED) {
             std::cerr << "Failed to get record configuration for file:" << files[i].filename << std::endl;
             break;
         }
 
-        if (files[i].record_config.wired_sync_mode == K4A_WIRED_SYNC_MODE_MASTER)
-        {
+        if (files[i].record_config.wired_sync_mode == K4A_WIRED_SYNC_MODE_MASTER) {
             std::cout << "Opened master recording file: " << files[i].filename << std::endl;
-            if (master_found)
-            {
+
+            if (master_found) {
                 std::cerr << "ERROR: Multiple master recordings listed!" << std::endl;
                 result = K4A_RESULT_FAILED;
                 break;
-            }
-            else
-            {
+            } else {
                 master_found = true;
                 master_id = i;
             }
-        }
-        else if (files[i].record_config.wired_sync_mode == K4A_WIRED_SYNC_MODE_SUBORDINATE)
-        {
+        } else if (files[i].record_config.wired_sync_mode == K4A_WIRED_SYNC_MODE_SUBORDINATE) {
             std::cout << "Opened subordinate recording file: " << files[i].filename << std::endl;
-        }
-        else
-        {
+        } else {
             std::cerr << "ERROR: Recording file was not recorded in master/sub mode: " << files[i].filename << std::endl;
             result = K4A_RESULT_FAILED;
             break;
         }
     }
 
-    if ((result == K4A_RESULT_SUCCEEDED) && master_found)
-    {
+    if ((result == K4A_RESULT_SUCCEEDED) && master_found) {
         std::cout << "Loading succeeded" << std::endl;
         int out_frames = 0;
         int skipped_frames = 0;
         first = true;
+
         while (!eof) {
             //get next valid frame for all cameras
             ok = prepare_next_valid_frame(&files[master_id]);
+
             if (eof) {
                 std::cout << "Last timestamp from " << files[master_id].filename << " = " << files[master_id].current_capture_timestamp << " | capture " << files[master_id].capture_id << std::endl;
                 break;
             }
-            if (!ok) return -1;
+
+            if (!ok) {
+                return -1;
+            }
+
             bool update_master = false;
-            for (size_t i = 0; i < file_count; i++)
-            {
-                if (i == master_id) continue; //we already read master frame
+
+            for (size_t i = 0; i < file_count; i++) {
+                if (i == master_id) {
+                    continue; //we already read master frame
+                }
+
                 int res = prepare_cond_next_valid_frame(&files[i], files[master_id].current_capture_timestamp, max_delay_cond);
+
                 if (res == -1) { //errors
                     if (eof) {
                         std::cout << "Last timestamp from " << files[i].filename << " = " << files[i].current_capture_timestamp << " | capture " << files[i].capture_id << std::endl;
                         break;
-                    }
-                    else {
+                    } else {
                         std::cerr << "errors " << i << std::endl;
                         return -1;
                     }
-                }else if (res == 2){ //we need to update master
+                } else if (res == 2) { //we need to update master
                     std::cerr << "Updating master | stats: " << files[i].filename << "\tcapture " << files[i].capture_id <<"\tt=" << files[i].current_capture_timestamp << "\t| master_t=" << files[master_id].current_capture_timestamp << std::endl;
                     update_master = true;
                     break;
                 }
             }
-            if (eof)
+
+            if (eof) {
                 break;
+            }
+
             if (update_master) {
                 skipped_frames++;
                 continue;
             }
+
             if (first) { //to start counting output/skipped frames from the first synced frame.
                 out_frames = 0;
                 skipped_frames = 0;
                 first = false;
             }
+
             out_frames++;
             /*std::cout << "succeeded" << std::endl; 
             for (size_t i = 0; i < file_count; i++)
@@ -276,25 +280,21 @@ int main(int argc, char* argv[])
 
         std::cout << "________________________________________________________________" << std::endl;
         std::cout << "RESULT : Succeded frames = " << out_frames << "\t| Skipped frames = " << skipped_frames << std::endl;
-    }
-    else 
-    {
+    } else {
         if (master_found) {
             std::cerr << "Loading failed" << std::endl;
-        }
-        else {
+        } else {
             std::cerr << "Loading failed -> Master not found" << std::endl;
         }
     }
 
-    for (size_t i = 0; i < file_count; i++)
-    {
-        if (files[i].handle != NULL)
-        {
+    for (size_t i = 0; i < file_count; i++) {
+        if (files[i].handle != NULL) {
             k4a_playback_close(files[i].handle);
             files[i].handle = NULL;
         }
     }
+
     free(files);
     return result == K4A_RESULT_SUCCEEDED ? 0 : 1;
 }
