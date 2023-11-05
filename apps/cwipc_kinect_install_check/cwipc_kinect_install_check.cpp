1fbbb77c205fe370a86e6e514557ceaba89ab2b8
Jack Jansen <Jack.Jansen@cwi.nl>
2023-11-05 23:04:06 +0100

    Added program cwipc_kinect_install_check which tries to load the cwipc_kinect shared library. Installs to libexec.

---
+++ apps/cwipc_kinect_install_check/cwipc_kinect_install_check.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include "string.h"
+#include <stdlib.h>
+#include <inttypes.h>
+#include "cwipc_util/api.h"
+#include "cwipc_kinect/api.h"
+
+int main(int argc, char** argv) {
+    char *error = NULL;
+    cwipc_source *generator = cwipc_kinect("auto", &error, CWIPC_API_VERSION);
+    char *expectedError = strstr(error, "no kinect cameras found");
+    if (expectedError == NULL) {
+        // Any other error is unexpected.
+        std::cerr << argv[0] << ": Error: " << error << std::endl;
+        return 1;
+    }
+    return 0;
+}
+
