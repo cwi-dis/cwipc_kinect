f5f25a3800c72e4be296c380fbe1aa10a82ea106
BEELZEBUB\DIS <cwidisgroup@gmail.com>
2023-11-06 01:14:30 +0100

    Fixed error in logic.

--- apps/cwipc_kinect_install_check/cwipc_kinect_install_check.cpp
+++ apps/cwipc_kinect_install_check/cwipc_kinect_install_check.cpp
@@ -9,11 +9,13 @@
 int main(int argc, char** argv) {
     char *error = NULL;
     cwipc_source *generator = cwipc_kinect("auto", &error, CWIPC_API_VERSION);
-    char *expectedError = strstr(error, "no kinect cameras found");
-    if (expectedError == NULL) {
-        // Any other error is unexpected.
-        std::cerr << argv[0] << ": Error: " << error << std::endl;
-        return 1;
+    if (generator == NULL) {
+        char* expectedError = strstr(error, "no kinect cameras found");
+        if (expectedError == NULL) {
+            // Any other error is unexpected.
+            std::cerr << argv[0] << ": Error: " << error << std::endl;
+            return 1;
+        }
     }
     return 0;
 }
