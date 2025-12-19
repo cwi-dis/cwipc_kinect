e11f6be7945fd4b31f725c349f2837aff09186d9
BEELZEBUB\jack <Jack.Jansen@cwi.nl>
2025-12-19 13:39:41 +0100

    Starting with documentation

--- src/K4ABaseCapture.hpp
+++ src/K4ABaseCapture.hpp
@@ -11,6 +11,11 @@
 
 #include "K4AConfig.hpp"
 
+/** Base class for capturers that are implemented with the K4A API
+ * 
+ * Type_api_camera: The type representing a single camera in the K4A API (k4a_device_t or k4a_playback_t)
+ * Type_our_camera: The type representing our wrapper around a single K4A camera (subclass of K4ABaseCamera)
+*/
 template<typename Type_api_camera, class Type_our_camera> class K4ABaseCapture : public CwipcBaseCapture {
 public:
     K4ACaptureConfig configuration; //!< Complete configuration read from cameraconfig.json
