b2c31410e14347001ad88acab27b4bae2f1e5154
Jack Jansen <Jack.Jansen@cwi.nl>
2026-01-28 11:46:28 +0100

    add --hardware flag to install-check programs.

--- apps/cwipc_kinect_install_check/cwipc_kinect_install_check.cpp
+++ apps/cwipc_kinect_install_check/cwipc_kinect_install_check.cpp
@@ -8,12 +8,16 @@
 
 int main(int argc, char** argv) {
     char *error = NULL;
+    bool want_hardware = argc > 1 && strcmp(argv[1], "--hardware") == 0;
     cwipc_source *generator = cwipc_kinect("auto", &error, CWIPC_API_VERSION);
     if (generator == NULL) {
         char* expectedError = strstr(error, "no cameras found");
         if (expectedError == NULL) {
             // Any other error is unexpected.
             std::cerr << argv[0] << ": Error: " << error << std::endl;
+            return 1;
+        }
+        if (want_hardware) {
             return 1;
         }
     }
