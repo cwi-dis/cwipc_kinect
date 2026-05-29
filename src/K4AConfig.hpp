d36da03c2afe453a90d36838cc2ea483f0ab5fcc
pbauszat <pablo.bauszat@cwi.nl>
2026-05-29 14:13:03 +0200

    Added meta-data that contains camera specifics such as focal length, principal point, and resolution (currently per-frame, but should not be).

--- src/K4AConfig.hpp
+++ src/K4AConfig.hpp
@@ -71,6 +71,19 @@ struct K4ACaptureMetadataConfig {
     bool want_rgb = false;
     bool want_depth = false;
     bool want_skeleton = false;
+    bool want_camera_specs = false;
+};
+
+struct K4ACaptureMetadataCameraSpecs {
+    // Focal length in pixels
+    float focal_length_x; 
+    float focal_length_y;
+    // Principal point (optical center) in pixels
+    float principal_point_x;
+    float principal_point_y;
+    // Color image size
+    unsigned int color_image_width;
+    unsigned int color_image_height;
 };
 
 struct K4ACaptureConfig : CwipcBaseCaptureConfig {
